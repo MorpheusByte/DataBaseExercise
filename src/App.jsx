825472a0f82f15073028d42dbe8fa8cfbe69c15f
MorpheusByte <tahasaidcakmak@icloud.com>
2025-05-21 21:18:33 +0300

    Appjsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,13 @@
-import './App.css'
-import Home from './pages/Home'
+import "bootstrap/dist/css/bootstrap.min.css";
+import Home from "./pages/Home";
+// index.html e de bootstrap in js teşekkürünü script olarak ekledik
 
 function App() {
-
   return (
-    <>
-      <Home/>
-    </>
-  )
+    <div>
+     <Home/>
+    </div>
+  );
 }
 
-export default App
+export default App;
